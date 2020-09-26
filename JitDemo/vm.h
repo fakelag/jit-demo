61207a7eb8647b67f6d23709b05775ed348f8c9f
fakelag <35497506+fakelag@users.noreply.github.com>
2020-09-26 23:49:38 +0300

    Document opcodes

--- JitDemo/vm.h
+++ JitDemo/vm.h
@@ -1,10 +1,24 @@
 #pragma once
 
-#define INTERP_ADD		1	// ADD r, r
-#define INTERP_SUB		2	// SUB r, r
-#define INTERP_LOAD		3	// LOAD r, long
-#define INTERP_LOOP		4	// LOOP <r if not zero>, rel_addr_backwards
-#define INTERP_RET		5	// RET r
+// ADD r, r
+// adds the second operand (register) the first operand (register), and stores the result in the first operand
+#define INTERP_ADD		1
+
+// SUB r, r
+// subtracts the second operand from the first operand, and stores the result in the first operand
+#define INTERP_SUB		2
+
+// LOAD r, long
+// loads a constant in the second operand to the first operand
+#define INTERP_LOAD		3
+
+// LOOP r, rel_addr_backwards
+// Checks if the value in the first operand is 0, if not, will jump back the second operand amount of bytes relative to the instruction
+#define INTERP_LOOP		4
+
+// RET r
+// Returns the value in the first operand
+#define INTERP_RET		5
 
 #define INTERP_R0 0
 #define INTERP_R1 1
